ba491695838ba931eb40ef651411463457840e38
RahulRN007 <rahulrnofficials@gmail.com>
2024-09-11 12:04:41 +0530

    folder structure created

---
+++ src/Components/Admin/AdminLogin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../Assets/Styles/AdminLogin.css'
+
+function AdminLogin() {
+  return (
+    <div>AdminLogin</div>
+  )
+}
+
+export default AdminLogin
---
+++ src/Components/Users/UserLogin.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../Assets/Styles/UserLogin.css'
+
+function UserLogin() {
+  return (
+    <div>UserLogin</div>
+  )
+}
+
+export default UserLogin
